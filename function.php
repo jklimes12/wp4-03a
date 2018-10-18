7b449903886f449e4387f7aaa9eeff17eefbffb2
jklimes12 <jklimes12@seznam.cz>
2018-10-18 15:45:57 +0200

    kruh

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+function obsah($r)
+{
+  $obsah= 3.14*$r*$r;
+ return $obsah;}
+
+function obvod($r)
+{
+  $obvod= 2*3.14*$r;
+  return $obvod;
+}
+
+ ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'function.php';
+$r = 3;
+
+$obvod= obvod($r);
+echo "obvod je: "$obvod;
+?></br><?php
+$obsah= obsah($r);
+echo "obsah je:" "$obsah;
+ ?>
